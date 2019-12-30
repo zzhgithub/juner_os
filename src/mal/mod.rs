90b012b5a3a9a0f09e3c94b9181ed04b85ebfc3e
zhouzihao <1042181618@qq.com>
2019-12-31 00:16:21 +0800

    tmp-求值临时提交

--- src/mal/mod.rs
+++ src/mal/mod.rs
@@ -8,6 +8,11 @@ pub mod reader;
 pub mod env;
 pub mod printer;
 
+use crate::mal::types::MalVal;
+use crate::mal::types::MalVal::{List};
+use crate::mal::types::MalRet;
+use crate::mal::env::Env;
+
 pub fn repl(){
     loop{
         //todo
@@ -20,5 +25,19 @@ pub fn rep(code:&str) {
     match ast {
         Ok(s)=>{println!("{}",s.pr_str(true))},
         Err(e)=>{println!("{}",format_error(e))}
+    }
+}
+
+// 求值
+fn eval(ast: MalVal, env: Env) -> MalRet {
+    match ast.clone(){
+        List(l,_)=>{
+            if l.len() == 0 {
+                return ast;
+            }
+            let a0 = &l[0];
+            // todo 处理list 调用第一个符号
+        },
+        //todo
     }
 }
